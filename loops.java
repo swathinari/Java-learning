9fd2283cc112fd0136a9d286b0dab8cafd5fa28f
swathinari <swathicomputer0128@gmail.com>
2025-03-12 09:28:09 +0530

    loops program

---
+++ loops.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Loop{
+    public static void main(String[] args) {
+        // for loop
+        int num =5;
+        for (int i = 0; i < 5; i=i+1) {
+            System.out.println(i);
+            
+        } 
+    System.out.println("----------------------");
+       // for each loop
+       List <Integer> number = Arrays.asList (10,20,30,40,50);
+       for (Integer x : number){
+        System.out.println(x);
+       }
+
+
+    }
+}
